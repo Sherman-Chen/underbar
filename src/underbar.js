36c4a81413fafe557c51b9e5f1bf1dfca52a06b1
Sherman <shc410@gmail.com>
2016-08-22 08:48:53 -0700

    Redo _.uniq and _.map, implement _.reduce

--- src/underbar.js
+++ src/underbar.js
@@ -113,8 +113,8 @@
       uniqs[val] = val;
     });
 
-    _.each(uniqs, function(elem) {
-      uniqList.push(elem);
+    _.each(uniqs, function(val) {
+      uniqList.push(val);
     });
 
     return uniqList;
@@ -126,13 +126,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
-    if (Array.isArray(collection)) {
       var results = [];
       for (var i = 0; i < collection.length; i++) {
         results.push(iterator(collection[i]));
       }
       return results;
-    }
   };
 
   /*
@@ -174,6 +172,16 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+      if (accumulator === undefined) {
+        accumulator = collection[0];
+        collection = collection.slice(1);
+      }
+
+      _.each(collection, function(item) {
+        accumulator = iterator(accumulator, item);
+      });
+
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
